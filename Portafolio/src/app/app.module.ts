b007909b8ecd78052aae1e6052842212473f430a
Nicolas-alt <a10100105555@gmail.com>
2020-07-30 12:56:49 -0500

    [Feat] Components

--- Portafolio/src/app/app.module.ts
+++ Portafolio/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    SkillsComponent,
+    ArticulosComponent
   ],
   imports: [
     BrowserModule,
