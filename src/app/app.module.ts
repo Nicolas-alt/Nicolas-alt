54660bd31392386ac8d34fdb00d575a8475904a4
Nicolas-alt <a10100105555@gmail.com>
2020-10-05 18:47:14 -0500

    delete(): Componentes no funcionales.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { SkillsComponent } from './components/skills/skills.component';
-import { ArticulosComponent } from './components/articulos/articulos.component';
 import { Page404Component } from './components/page404/page404.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +16,9 @@ import { ButtonsComponent } from './components/buttons/buttons.component';
     FooterComponent,
     HomeComponent,
     SkillsComponent,
-    ArticulosComponent,
     Page404Component,
     ExperienciaComponent,
     NavBarComponent,
-    ButtonsComponent
-  ],
-  imports: [
     BrowserModule,
     AppRoutingModule
   ],
