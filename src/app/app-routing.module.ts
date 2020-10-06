5eb3c4225b5aa425266943159f140b02de292b1d
Nicolas-alt <a10100105555@gmail.com>
2020-10-05 20:10:55 -0500

    { update } app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page404Component } from './components/page404/page404.component';
 import {ExperienciaComponent} from './components/experiencia/experiencia.component'
 import { SkillsComponent } from './components/skills/skills.component';
-import { ArticulosComponent } from './components/articulos/articulos.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path: 'experiencia', component: ExperienciaComponent},
   {path: 'skills', component: SkillsComponent},
-  {path: 'articulos', component: ArticulosComponent},
   {path: '', component: HomeComponent},
   {path: '**', component: Page404Component}
 ];
