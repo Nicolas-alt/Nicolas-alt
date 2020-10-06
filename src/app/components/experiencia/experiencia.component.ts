f706c3c030593df81c3dc6c872409f95e07c910d
Nicolas-alt <a10100105555@gmail.com>
2020-10-05 19:20:05 -0500

    { update } = experiencia component .scss

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
-  styleUrls: ['./experiencia.component.sass']
+  styleUrls: ['./experiencia.component.scss']
 })
 export class ExperienciaComponent implements OnInit {
     goToLink(url: string){
       window.open(url, "_blank");
     }
+    
   constructor() { }
 
   ngOnInit(): void {
