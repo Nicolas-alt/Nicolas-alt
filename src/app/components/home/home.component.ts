5c861a6747564ca3d28a630bfc5f3b1b07f45c93
Nicolas-alt <a10100105555@gmail.com>
2020-10-09 16:41:51 -0500

    { feat } type code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  Typed  from 'typed.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let typed = new Typed("#typed", {
+      strings: ['git push --force ^1000\n `pushed to origin with option force`'],
+      smartBackspace: true, 
+      loop: true,
+      typeSpeed: 75,
+      backSpeed: 50,
+  });
   }
 
 }
