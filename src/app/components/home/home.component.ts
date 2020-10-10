237aa3fc97f808404318a532f5b64b41a7aa5639
Nicolas-alt <a10100105555@gmail.com>
2020-10-09 19:29:40 -0500

    { feat } animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,11 +12,25 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     let typed = new Typed("#typed", {
-      strings: ['git push --force ^1000\n `pushed to origin with option force`'],
+      strings: [`
+      <i style="color:  #3498DB"> export </i>
+      <span style="color: #8E44AD">const </span>
+      <span style="color: white"> greeting </span> = 
+     <i style="color: #8E44AD"> () </i> 
+      =>
+       { <br />
+       <span style="color: #8E44AD"> return </span> (
+       <span style="color: #C0392B">  < h1> </span>
+       <span style="color: white">   Hi there!! </span>
+       <span style="color: #C0392B"> < /h1> </span>
+         ); <br />
+       }
+    `],
       smartBackspace: true, 
-      loop: true,
-      typeSpeed: 75,
+      loop: false,
+      typeSpeed: 25,
       backSpeed: 50,
+      startDelay: 1000
   });
   }
 
