986aa2197ec656470b33aec3bab9be413a4f271e
Nicolas-alt <a10100105555@gmail.com>
2020-10-13 10:41:41 -0500

    { feat } maquetación

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -9,10 +9,58 @@ export class ExperienciaComponent implements OnInit {
     goToLink(url: string){
       window.open(url, "_blank");
     }
-    
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    projects:any[] = [
+      {
+        title: 'Logbook',
+        class: 'lb',
+        description: 'Landing',
+        image: '../../../assets/img/lg.png',
+        url: ''
+      },
+      {
+        title: 'Rails Blog',
+        class: 'rail',
+        description: 'Landing',
+        image: '../../../assets/img/home.png',
+        url: ''
+      },
+      {
+        title: 'Arduino Page',
+        class: 'ard',
+        description: 'Landing',
+        image: '../../../assets/img/ard.png',
+        url: ''
+      },
+      {
+        title: 'Room',
+        class: 'room',
+        description: 'Single page challenge',
+        image: '../../../assets/img/room.jpg',
+        url: ''
+      },
+      {
+        title: 'Book Mark',
+        class: 'book',
+        description: 'Single page challenge',
+        image: '../../../assets/img/bookMark.jpg',
+        url: ''
+      },
+      {
+        title: 'Fylo',
+        class: 'fylo',
+        description: 'Landing page challenge',
+        image: '../../../assets/img/fylo.png',
+        url: ''
+      }
+   ];
+   //<!--
+  // <a (click)="goToLink('https://log-book-landing.vercel.app/')" class="btn btn-primary">Ver</a>
+  // <a (click)="goToLink('https://github.com/Nicolas-alt/LogBookLanding')"  class="btn btn-outline-info">Ver código</a>
+  ///<a (click)="goToLink('https://github.com/Nicolas-alt/RailsBlog')" class="btn btn-outline-info">Ver código</a>
+ //  <a (click)="goToLink('https://github.com/Nicolas-alt/DiscoveringMusic')" class="btn btn-outline-info">Ver código</a>
+ //  <a (click)="goToLink('https://github.com/Nicolas-alt/The-tables')" class="btn btn-outline-info">Ver código</a>
+ // -->
+  constructor() {}
+  ngOnInit(): void {}
 
 }
