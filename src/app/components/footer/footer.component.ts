09d7591830eb4802a18e10667b7d338b2f51d8d8
Nicolas-alt <a10100105555@gmail.com>
2020-10-13 11:03:39 -0500

    { feat } footer animation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import ScrollReveal from 'scrollreveal'
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +11,15 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const icons = ScrollReveal({ reset: true });
 
+     icons.reveal('.a--media',{
+      opacity: 0,
+      delay: 1000,
+      duration: 300,
+      interval: 250,
+      rotate: {x:20, y:50 } ,
+      origin: 'left'
+    }, 50)
+  }
 }
