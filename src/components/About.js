2dcd2bea192a19ecf8ad9517ab45b493eef27deb
Nicolas-alt <a10100105555@gmail.com>
2021-01-17 15:44:07 -0500

    Pretty ( Code xd )

--- src/components/About.js
+++ src/components/About.js
@@ -5,15 +5,14 @@ import '../assets/sass/About.scss'
 const About = _ => {
     return (
         <>
-        <Helmet>
-            <title>About Me</title>
-            <meta name="description" content="Un vistazo acerca de mi vida profesional." />
-            <meta name="theme-color" content="#0545" />
-        </Helmet>
-        <section className="section--about">
-            About
-        </section>
-        
+            <Helmet>
+                <title>About Me</title>
+                <meta name="description" content="Un vistazo acerca de mi vida profesional." />
+                <meta name="theme-color" content="#0545" />
+            </Helmet>
+            <section className="section--about">
+                About
+            </section>
         </>
     )
 }
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,15 +6,14 @@ import '../assets/sass/Experience.scss'
 export const Experience = _ => {
   return (
     <>
-       <Helmet>
-            <title>My experience</title>
-            <meta name="description" content="Mi experiencia como front dev" />
-            <meta name="theme-color" content="#ffff" />
-        </Helmet>
-    <section className="section--experience">
-      Experience 
-    </section>
-  
+      <Helmet>
+          <title>My experience</title>
+          <meta name="description" content="Mi experiencia como front dev" />
+          <meta name="theme-color" content="#ffff" />
+      </Helmet>
+      <section className="section--experience">
+        Experience 
+      </section>
     </>
   )
 }
