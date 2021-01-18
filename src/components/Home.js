0531af8cc16d429c104fe8fc31a0a1bbe5847be2
Nicolas-alt <a10100105555@gmail.com>
2021-01-18 17:37:40 -0500

    Feat  ( Typed text )

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Fade from 'react-reveal/Fade';
+import Typed from 'typed.js'
 
 import '../assets/sass/Home.scss';
 import CustomHelmet from './CustomHelmet';
 
 const Home = _ => {
+  useEffect(()=>{
+    let dev = new Typed("#dev",{
+      strings: [
+        "Front dev 🎨",
+        "Desarrollador 🧩",
+        "Entusiasta 🤩",
+        "Developer 👾",
+        "Web Developer 💻" 
+    ],
+      loop: true,
+      startDelay: 1000,
+      typeSpeed: 80,
+      backSpeed: 80
+    });
+  },[])
+  
 
   return (
      <Fade bottom>
@@ -16,7 +33,7 @@ const Home = _ => {
         <section className="section--home">  
           <p>Hi there, i am</p>
           <h2>Nicolas Jiménez</h2>
-          <p>I'm a <span></span> </p>
+          <p>I'm a <span id="dev"></span> </p>
         </section>
      </Fade>
   );
